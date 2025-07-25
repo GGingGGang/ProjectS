08c19ca005ea9abe4f9d03584ce726d24738114b
GGingGGang <assignment1@ggingggang.shop>
2025-07-26 02:50:58 +0900

    add PlayerMovement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public float screenHalfWidthInWorldUnits = 3.6f;
+
+    private float spriteHalfWidth;
+
+    void Start() {
+        spriteHalfWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
+    }
+
+    void Update() {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(inputX, 0, 0) * moveSpeed * Time.deltaTime;
+        transform.position += movement;
+
+        float clampedX = Mathf.Clamp(transform.position.x, -screenHalfWidthInWorldUnits + spriteHalfWidth, screenHalfWidthInWorldUnits - spriteHalfWidth);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+    }
+}
