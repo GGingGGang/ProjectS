a27f9490359becf56749c357cf514c90501d9370
GGingGGang <assignment1@ggingggang.shop>
2025-07-28 10:08:57 +0900

    fix Enemy.cs

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -11,5 +11,6 @@ public class Enemy : MonoBehaviour {
         int offsetPixel = GameManager.EnemyMoveTime -8;
         float offsetWorld = offsetPixel / 10f; 
         transform.position = basePosition + new Vector3(offsetWorld, 0f, 0f);
+
     }
 }
