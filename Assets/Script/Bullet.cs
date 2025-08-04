1f00e759ab9f507f66b6e7d1d2c19bb48bd578aa
GGingGGang <assignment1@ggingggang.shop>
2025-08-04 10:05:43 +0900

    피격

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,4 +4,14 @@ public class Bullet : MonoBehaviour {
     void OnBecameInvisible() {
         Destroy(gameObject);
     }
-}
+    private void OnTriggerEnter2D(Collider2D collision) {
+        // 적 유닛에 맞으면
+        if (collision.CompareTag("Enemy")) {
+            // 적 오브젝트 제거
+            Destroy(collision.gameObject);
+
+            // 총알 제거
+            Destroy(gameObject);
+        }
+    }
+}   
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
 
 public class GameManager : MonoBehaviour {
     public static int EnemyMoveTime = 0;
@@ -7,8 +11,18 @@ public class GameManager : MonoBehaviour {
     private float timer = 0f;
     public float moveInterval = 0.5f; // 몇 초마다 이동할지 결정
 
+    public GameObject enemyPrefab; // Inspector에서 Enemy 프리팹 드래그
+
+    private List<Vector2> spawnPoints = new List<Vector2>();
+    private List<Vector2> usedPoints = new List<Vector2>();
+    
+    void Start() {
+        InitSpawnPoints();
+        SpawnEnemies(5); // 원하는 수 만큼 적군 생성
+    }
     void Update() {
         timer += Time.deltaTime;
+        int enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
 
         if (timer >= moveInterval) {
             timer = 0f;
@@ -21,5 +35,39 @@ public class GameManager : MonoBehaviour {
 
             Debug.Log("EnemyMoveTime: " + EnemyMoveTime);
         }
+
+        if (enemyCount < 1) {
+            SpawnEnemies(30);
+        }
     }
+
+
+    void InitSpawnPoints() {
+        for (float x = -3.325f; x <= 3.325f; x += 0.5f) {
+            for (float y = 1f; y <= 3f; y += 1f) {
+                spawnPoints.Add(new Vector2(x, y));
+            }
+        }
+    }
+
+    void SpawnEnemies(int count) {
+        usedPoints.Clear();
+
+        for (int i = 0; i < count; i++) {
+            Vector2 point = GetRandomAvailablePoint();
+            if (point != Vector2.zero) {
+                Instantiate(enemyPrefab, point, Quaternion.Euler(0, 0, 180)); // 180도 회전
+                usedPoints.Add(point);
+            }
+        }
+    }
+
+    Vector2 GetRandomAvailablePoint() {
+        List<Vector2> available = spawnPoints.Except(usedPoints).ToList();
+        if (available.Count == 0) return Vector2.zero;
+
+        int rand = Random.Range(0, available.Count);
+        return available[rand];
+    }
+
 }
